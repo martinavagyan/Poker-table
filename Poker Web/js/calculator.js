b907f2db3d8d49b30ba95add4a31a6553aa5eace
martinavagyan <martinavagyan@gmail.com>
2016-07-13 15:12:01 +0200

    whole project

---
+++ Poker Web/js/calculator.js
@@ -0,0 +1,128 @@
+var cards ={
+	cardsAll: function(){
+		var a ={'c':['2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AC'],
+				'd':['2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD','AD'],
+				'h':['2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AH'],
+				's':['2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS','AS']};
+		return a;
+		},
+	
+	cardValue: function(){			
+		var a ={'2C':1,'3C':2,'4C':3,'5C':4,'6C':5,'7C':6,'8C':7,'9C':8,'10C':9,'JC':10,'QC':11,'KC':12,'AC':13,
+				'2D':1,'3D':2,'4D':3,'5D':4,'6D':5,'7D':6,'8D':7,'9D':8,'10D':9,'JD':10,'QD':11,'KD':12,'AD':13,
+				'2H':1,'3H':2,'4H':3,'5H':4,'6H':5,'7H':6,'8H':7,'9H':8,'10H':9,'JH':10,'QH':11,'KH':12,'AH':13,
+				'2S':1,'3S':2,'4S':3,'5S':4,'6S':5,'7S':6,'8S':7,'9S':8,'10S':9,'JS':10,'QS':11,'KS':12,'AS':13};
+		return a;
+		},				
+	cardOnTable: function(){
+		var remain = [];
+		remain['c'] = arr_diff(cards.cardsAll['c'], Check.cards['c']);
+		remain['d'] = arr_diff(cards.cardsAll['d'], Check.cards['d']);
+		remain['h'] = arr_diff(cards.cardsAll['h'], Check.cards['h']);
+		remain['s'] = arr_diff(cards.cardsAll['s'], Check.cards['s']);
+		return remain;
+	},
+	tableCards: function(){
+		var arrayTable = [];
+		var n = 1;
+		for(var i =1;i<7;i++){
+			arrayTable['table'+i] = [document.getElementById('card'+n).children[0].id,document.getElementById('card'+(n+1)).children[0].id];			
+			n = n+2;
+		}
+		console.log(arrayTable);
+		return arrayTable;
+	},
+};
+function highCard()
+{	
+	/*{'table1':['4D','9S'],'table2':['5D','KS'],'table3':['2D','JS'],
+				 'table4':['JD','JC'],'table5':['AC','5S'],'table6':['AD','AS'],				 
+				};*/
+	var cardsInTable = cards.tableCards();	
+	var index1 = undefined;
+	var index2 = undefined;
+	var values = cards.cardValue();
+	
+	
+	for(var i = 1;i<7;i++)
+	{
+		index1 = cardsInTable['table'+i][0];
+		index2 = cardsInTable['table'+i][1];
+		
+		if(values[index1] > values[index2])
+			cardsInTable['table'+i] =values[index1];
+		else cardsInTable['table'+i] =values[index2];			
+	}
+	var returnArray = [];
+	var sortedArray = [];
+	for(z in cardsInTable){	
+		returnArray.push(cardsInTable[z]);
+	}
+	for(z in cardsInTable){	
+		sortedArray.push(cardsInTable[z]);
+	}	
+	
+	sortedArray.sort(function(a, b){return b-a});
+	
+	console.log(sortedArray);	
+	console.log(returnArray);
+	
+	var n = 6;
+	var finalArray = [];
+	for(var i = 0;i<sortedArray.length; i++)
+	{				
+		
+		finalArray [returnArray.indexOf(sortedArray[i])] = n;
+		returnArray[returnArray.indexOf(sortedArray[i])] = 0;
+		//splice(2, 0, "Lene");
+		if(sortedArray[i]!= sortedArray[i+1])
+		n--;
+	}	
+	console.log(finalArray);
+	return finalArray;
+};
+
+function onePair()
+{
+	var cardsInTable = {'table1':['4D','9S'],'table2':['5D','KS'],'table3':['2D','JS'],
+						'table4':['JD','JC'],'table5':['AC','5S'],'table6':['AD','AS'],				 
+						};
+	var index1 = undefined;
+	var index2 = undefined;
+	var values = cards.cardValue();
+	var havePairArray = [];
+	var noPairArray = [];
+	
+	for(var i = 1;i<7;i++)
+	{
+		index1 = cardsInTable['table'+i][0];
+		index2 = cardsInTable['table'+i][1];
+		
+		if(values[index1] == values[index2])
+			havePairArray.push('table'+i);
+		else noPairArray.push('table'+i);
+	}
+	//if one pare exists compare with others and find the highest, if does not exist chack the probability
+	
+	//highest from ones that have
+	
+	console.log(cardsInTable);
+	console.log(havePairArray);
+	console.log(noPairArray);
+	
+	
+}
+
+//diffence between array
+function arr_diff(a1, a2)
+{
+  var a=[], diff=[];
+  for(var i=0;i<a1.length;i++)
+    a[a1[i]]=true;
+  for(var i=0;i<a2.length;i++)
+    if(a[a2[i]]) delete a[a2[i]];
+    else a[a2[i]]=true;
+  for(var k in a)
+    diff.push(k);
+  return diff;
+};
---
+++ Poker Web/js/custom.js
@@ -0,0 +1,151 @@
+//initaly I created chack to check if the charachters are specified
+var Check ={
+		GiveIdToTableCards: function(tableCardElem,cardId)
+		{
+			tableCardElem.setAttribute('id',cardId);
+			var cardType = cardId.slice(-1);
+			cardType = cardType.toLowerCase();
+			
+			var index = Check.cards[cardType].indexOf(cardId);
+			if (index > -1)			
+				Check.cards[cardType].splice(index, 1);			
+		},
+		classOfParent: '',
+		findTableOfCard: function(inputElemen){
+			if(hasClass(inputElemen.parentElement.parentElement,'headerCards') == true)
+				Check.classOfParent = 'headerImg';
+			else if(hasClass(inputElemen.parentElement.parentElement,'tableCards') == true)
+				Check.classOfParent = 'footerImg';
+		},
+		activeSearchBox: undefined,		
+		currentInput: '',
+		name: "martin",
+		cards: {'c':['2C','3C','4C','5C','6C','7C','8C','9C','10C','JC','QC','KC','AC'],
+				'd':['2D','3D','4D','5D','6D','7D','8D','9D','10D','JD','QD','KD','AD'],
+				'h':['2H','3H','4H','5H','6H','7H','8H','9H','10H','JH','QH','KH','AH'],
+				's':['2S','3S','4S','5S','6S','7S','8S','9S','10S','JS','QS','KS','AS']},
+		//enter function
+		readyEnter: function(inputEl)
+		{
+			var InputValue = convertToArrayFormat(inputEl.value);
+			if(isInArray(InputValue, Check.cards[inputEl.value.charAt(0) ]))
+				return true;
+			else return false;
+		},
+		pressEnter: function(e,inputElem){
+			 if (e.keyCode == 13 && Check.readyEnter(inputElem) == true) {	
+					//converting to desired format
+					var searchedCardClass = convertToArrayFormat(inputElem.value);
+					var parentElem = inputElem.parentElement.parentElement;
+					var choosenCard = document.createElement('div');
+					choosenCard.setAttribute('class',Check.classOfParent+" D"+searchedCardClass);
+					Check.GiveIdToTableCards(choosenCard,searchedCardClass);
+					choosenCard.innerHTML = '<span class="removeBtn" onclick="Check.closeChoosen(this)"></span>';
+					parentElem.innerHTML = '';
+					parentElem.appendChild(choosenCard);	
+					
+				}
+			},			
+		closeChoosen: function(closeBtn){
+			Check.cards[closeBtn.parentElement.id.slice(-1).toLowerCase()].push(closeBtn.parentElement.id);//from the spcified array push the id of the removed card
+			closeBtn.parentElement.parentElement.innerHTML = 
+			'<div class="cardAdd"><input type="text" maxlength="2" placeholder="Choose" onkeypress="return Check.pressEnter(event,this)" onkeyup = "Check.chooseCard(this)"></input></div>';
+			
+		},
+		
+		//this is the engine for searchBox
+		chooseCard: function(inputElem){
+			Check.findTableOfCard(inputElem);
+			if  (typeof Check.activeSearchBox != 'undefined')  Check.activeSearchBox.remove();//deleting previus searchbox.
+			var srchedCrdTpe = inputElem.value;
+			var searchContainer = document.createElement('div');
+			searchContainer.setAttribute('id','searchContainer');			
+			inputElem.parentElement.appendChild(searchContainer);	
+			searchContainer = document.getElementById('searchContainer');
+			Check.activeSearchBox = searchContainer; // getting current active search box.		
+			//if the text is one charachered and is D C H S
+			if(srchedCrdTpe.length == 0)
+			{
+				searchContainer.innerHTML = '';
+			}
+			else if(srchedCrdTpe.length == 1 && (srchedCrdTpe =='d' || srchedCrdTpe =='s' || srchedCrdTpe =='h' || srchedCrdTpe =='c'))	
+			{
+				searchContainer.innerHTML =
+				"<div class='lifeSearchStyle'></div><div id = 'searchBox' class='liveSearch'></div>";
+				for (var i = 0; i < Check.cards[srchedCrdTpe].length;i++)
+				{	
+					var searchBox = document.getElementById('searchBox');	
+					var searchedCard = Check.cards[srchedCrdTpe][i];	
+					var element = document.createElement('div');
+					element.setAttribute('class','searchCards D'+searchedCard);
+					element.setAttribute('id','SSC'+searchedCard);//giving the id to the small searched cards (ssc)
+					element.onclick = function(e){
+						var parentTable = element.parentElement.parentElement.parentElement.parentElement;
+						var choosenCard = document.createElement('div');
+						var SSCId = e.target.id.replace("SSC","");
+						choosenCard.setAttribute('class',Check.classOfParent+' D'+SSCId);
+						Check.GiveIdToTableCards(choosenCard,SSCId);
+						choosenCard.innerHTML = '<span class="removeBtn" onclick="Check.closeChoosen(this)"></span>';
+						parentTable.innerHTML = '';
+						parentTable.appendChild(choosenCard);							
+					};					
+					searchBox.appendChild(element);
+				}	
+			}
+			else if (srchedCrdTpe.length == 2 && Check.readyEnter(inputElem) == true)				
+			{
+				srchedCrdTpe = convertToArrayFormat(srchedCrdTpe);
+				
+				searchContainer.innerHTML = '';
+				searchContainer.innerHTML = "<div class='lifeSearchStyle'></div><div id = 'searchBox' class='liveSearch liveSearchSecondString'></div>";				
+				var searchBox = document.getElementById('searchBox');						
+				var element = document.createElement('div');
+				element.setAttribute('class','searchCards D'+srchedCrdTpe);
+				element.setAttribute('id','SSC'+searchedCard);//giving the id to the small searched cards (ssc)
+				element.onclick = function(e){
+					var parentTable = element.parentElement.parentElement.parentElement.parentElement;
+					var choosenCard = document.createElement('div');
+					choosenCard.setAttribute('class',Check.classOfParent+' D'+srchedCrdTpe);
+					Check.GiveIdToTableCards(choosenCard,srchedCrdTpe);
+					choosenCard.innerHTML = '<span class="removeBtn" onclick="Check.closeChoosen(this)"></span>';
+					parentTable.innerHTML = '';
+					parentTable.appendChild(choosenCard);							
+				};					
+				searchBox.appendChild(element);
+			}
+			else
+			{
+				searchContainer.innerHTML = '';
+			}
+			
+			
+			
+		},
+	};
+
+
+
+//Additional Functions 
+//1 Reverse
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+//end reverse
+//has Class
+function hasClass(element, className) {
+    return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
+}
+//end has class
+//is in array
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+//end is in array
+//format of array =cards
+function convertToArrayFormat(inputElem)
+{
+
+	var searchedCardClass = inputElem.toUpperCase();
+	return reverse(searchedCardClass);//end converting
+}
+//end
